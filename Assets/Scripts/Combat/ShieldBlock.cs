2d7d71b5b86e68b2d12759a9fd495412617b397a
uutahan <umut.utku.tahan@gmail.com>
2021-03-02 23:05:59 +0300

    Pan hit effect added.

--- Assets/Scripts/Combat/ShieldBlock.cs
+++ Assets/Scripts/Combat/ShieldBlock.cs
@@ -4,11 +4,22 @@ namespace SwordShield.Combat
 {
     public class ShieldBlock : MonoBehaviour
     {
+        [SerializeField]
+        private ParticleSystem panEffect = null;
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.GetComponent<Projectile>() == null) return;
 
             Destroy(other.gameObject);
+
+            if (panEffect != null)
+            {
+                Vector3 rot = other.transform.rotation.eulerAngles;
+                //rot = new Vector3(rot.x, rot.y + 180, rot.z);
+                Instantiate(panEffect, other.transform.position, Quaternion.Euler(rot));
+
+            }
         }
     }
 }
