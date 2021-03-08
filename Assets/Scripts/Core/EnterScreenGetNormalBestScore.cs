6fab88649942a8b1bb05ec1d0ca3239be92f183f
uutahan <umut.utku.tahan@gmail.com>
2021-03-09 00:03:48 +0300

    start menu best scores added.

---
+++ Assets/Scripts/Core/EnterScreenGetNormalBestScore.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace SwordShield.Core
+{
+    public class EnterScreenGetNormalBestScore : MonoBehaviour
+    {
+        private TMPro.TMP_Text normalBestScore;
+
+        void Start()
+        {
+            normalBestScore = GetComponent<TMPro.TextMeshProUGUI>();
+
+            int bestScore = PlayerPrefs.GetInt("NormalBestScore", 0);
+            normalBestScore.text = "BEST SCORE: " + bestScore.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/Core/EnterScreenGetSurvivalBestScore.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace SwordShield.Core
+{
+    public class EnterScreenGetSurvivalBestScore : MonoBehaviour
+    {
+        private TMPro.TMP_Text survivalBestScore;
+
+        void Start()
+        {
+            survivalBestScore = GetComponent<TMPro.TextMeshProUGUI>();
+
+            int bestScore = PlayerPrefs.GetInt("SurvivalBestScore", 0);
+            survivalBestScore.text = "BEST SCORE: " + bestScore.ToString();
+        }
+    }
+}
